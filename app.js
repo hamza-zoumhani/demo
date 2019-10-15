d7c04984fc1da37d7600331cb8a267da35ad3c7e
Hamza Zoumhani <zoumhanihamza@gmail.com>
2019-10-15 00:11:35 -0500

    added post, trying to work on more

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/demo");
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/index.html");
+});
+
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+
+var User = mongoose.model("User", nameSchema);
+
+app.get('/addname', (req, res)=>{
+    User.find({}).exec((err, data)=>{
+        res.json(data);
+    });
+});
+
+app.post("/addname", (req, res)=>{
+    var myData = new User(req.body);
+    myData.save()
+    .then(item=>{
+        res.send("Item saved to database");
+    })
+    .catch(err=> {
+        res.status(400).send("Unable to seave to database");
+    });
 });
 
 app.listen(port, () => {
